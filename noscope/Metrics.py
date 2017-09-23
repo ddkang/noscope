63f59fd4b0d4e1816bb97cbb1b725005f495f6ad
Daniel Kang <daniel.d.kang@gmail.com>
2017-09-23 13:35:46 -0700

    Non-crashing, but also non-functional detection
    TODO:
    - Compute metrics only where appropriate
    - Weight metrics (mse, iou) by when they are actually computed
    - Put metrics on same scale (classification, mse)

--- noscope/Metrics.py
+++ noscope/Metrics.py
@@ -1,88 +1,49 @@
+import sklearn
+import sklearn.metrics
+import StatsUtils
+import np_utils
 
-import cv2
-import numpy as np
-from skimage.measure import compare_psnr, compare_ssim, compare_nrmse
+def classification_metrics(proba, Y_test):
+    predicted_labels = np_utils.probas_to_classes(proba)
+    true_labels = np_utils.probas_to_classes(Y_test)
+    precision, recall, fbeta, support = \
+        sklearn.metrics.precision_recall_fscore_support(predicted_labels, true_labels)
+    accuracy = sklearn.metrics.accuracy_score(predicted_labels, true_labels)
 
+    num_penalties, thresh_low, thresh_high = \
+        StatsUtils.yolo_oracle(Y_test[:, 1], proba[:, 1])
+    windowed_acc, windowed_supp = StatsUtils.windowed_accuracy(predicted_labels, Y_test)
+    confusion = sklearn.metrics.confusion_matrix(true_labels, predicted_labels)
 
-is_version3 = cv2.__version__[0] == '3'
-class Metrics(dict):
-    @staticmethod
-    def compute_chisq(hist1, hist2):
-        if is_version3:
-            return cv2.compareHist(hist1, hist2, method=cv2.HISTCMP_CHISQR)
-        else:
-            return cv2.compareHist(hist1, hist2, method=cv2.cv.CV_COMP_CHISQR)
+    metrics = {'precision': precision,
+               'recall': recall,
+               'fbeta': fbeta,
+               'support': support,
+               'accuracy': accuracy,
+               'penalities': num_penalties,
+               'windowed_accuracy': windowed_acc,
+               'windowed_support': windowed_supp}
+    return metrics
 
-    @staticmethod
-    def chisq(a, b):
-        def get_hist(frame, nb_bins=32):
-            hist = np.concatenate(map(
-                    lambda i: cv2.calcHist([frame], [i], None, [nb_bins], [-1, 1]),
-                    xrange(3)))
-            hist = cv2.normalize(hist, hist)
-            return hist
-        ha = get_hist(a)
-        hb = get_hist(b)
-        return Metrics.compute_chisq(ha, hb)
-
-    @staticmethod
-    def absdiff(a, b):
-        return np.sum(np.abs(a - b))
-
-    @staticmethod
-    def mse(a, b):
-        return np.sum((a - b) ** 2) / float(a.size)
-
-    @staticmethod
-    def ssim(a, b):
-        return compare_ssim(a, b, multichannel=True)
-
-    @staticmethod
-    def nrmse_euc(a, b):
-        return compare_nrmse(a, b, norm_type='Euclidean')
-
-    @staticmethod
-    def nrmse_minmax(a, b):
-        return compare_nrmse(a, b, norm_type='min-max')
-
-    @staticmethod
-    def nrmse_mean(a, b):
-        return compare_nrmse(a, b, norm_type='mean')
-
-    @staticmethod
-    def psnr(a, b):
-        psnr_val = -1 * compare_psnr(a, b)
-        if np.isnan(psnr_val):
-            psnr_val = 10
-        if np.isinf(psnr_val):
-            psnr_val = -10
-        return psnr_val
-
-    @staticmethod
-    def chisq_3d(a, b):
-        def get_hist(frame):
-            tmp = frame
-            hist = cv2.calcHist([tmp], [0, 1, 2], None, [8, 8, 8], [-1, 1] * 3)
-            hist = hist.flatten()
-            hist = cv2.normalize(hist, hist)
-            return hist
-        ha = get_hist(a)
-        hb = get_hist(b)
-        return Metrics.compute_chisq(ha, hb)
-
-    def __init__(self, *args):
-        dict.__init__(self, args)
-
-        self.metrics = \
-            [(self.absdiff, 'absdiff'),
-             (self.mse, 'mse'),
-             (self.chisq, 'chisq'),
-             (self.psnr, 'psnr'),
-             (self.nrmse_euc, 'nrmse-euc'),
-             (self.nrmse_minmax, 'nrmse-minmax'),
-             (self.nrmse_mean, 'nrmse-mean'),
-             (self.chisq_3d, 'chisq-3d')]
-
-        for metric, name in self.metrics:
-            self[name] = metric
-
+def box_iou(box1, box2):
+    def to_cent(box):
+        xmin, ymin, xmax, ymax = box
+        xcent = (xmax + xmin) / 2
+        ycent = (ymax + ymin) / 2
+        return (xcent, ycent, xmax - xcent, ymax - ycent)
+    box1 = to_cent(box1)
+    box2 = to_cent(box2)
+    if box1[0] + box1[2] <= box2[0] - box2[2] or \
+            box2[0] + box2[2] <= box1[0] - box1[2] or \
+            box1[1] + box1[3] <= box2[1] - box2[3] or \
+            box2[1] + box2[3] <= box1[1] - box1[3]:
+        return 0.0
+    else:
+        xA = min(box1[0] + box1[2], box2[0] + box2[2])
+        yA = min(box1[1] + box1[3], box2[1] + box2[3])
+        xB = max(box1[0] - box1[2], box2[0] - box2[2])
+        yB = max(box1[1] - box1[3], box2[1] - box2[3])
+        interArea = (xA - xB) * (yA - yB)
+        box1Area = (2 * box1[2]) * (2 * box1[3])
+        box2Area = (2 * box2[2]) * (2 * box2[3])
+        return max(interArea / float(box1Area + box2Area - interArea), 0.0)
--- noscope/Models.py
+++ noscope/Models.py
@@ -1,3 +1,4 @@
+import enum
 import sklearn
 import sklearn.metrics
 import time
@@ -5,7 +6,9 @@ import os
 import tempfile
 import StatsUtils
 import DataUtils
+import Metrics
 import numpy as np
+import keras
 import keras.optimizers
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras.models import Sequential
@@ -17,22 +20,28 @@ import np_utils
 def generate_conv_net_base(
         input_shape, nb_classes,
         nb_dense=128, nb_filters=32, nb_layers=1, lr_mult=1,
-        kernel_size=(3, 3), stride=(1, 1)):
+        kernel_size=(3, 3), stride=(1, 1),
+        use_bn=True):
     assert nb_layers >= 1
     model = Sequential()
     model.add(Conv2D(nb_filters, kernel_size,
-                     strides=stride,
-                     padding='same',
-                     input_shape=input_shape,
-                     activation='relu'))
-    model.add(Conv2D(nb_filters, (3, 3), padding='same', activation='relu'))
+                     strides=stride, padding='same', input_shape=input_shape))
+    if use_bn: model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Conv2D(nb_filters, (3, 3), padding='same'))
+    if use_bn: model.add(BatchNormalization())
+    model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
     for i in range(1, nb_layers):
         factor = 2 ** i
-        model.add(Conv2D(nb_filters * factor, (3, 3), padding='same', activation='relu'))
-        model.add(Conv2D(nb_filters * factor, (3, 3), padding='same', activation='relu'))
+        model.add(Conv2D(nb_filters * factor, (3, 3), padding='same'))
+        if use_bn: model.add(BatchNormalization())
+        model.add(Activation('relu'))
+        model.add(Conv2D(nb_filters * factor, (3, 3), padding='same'))
+        if use_bn: model.add(BatchNormalization())
+        model.add(Activation('relu'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(Dropout(0.25))
 
@@ -42,15 +51,31 @@ def generate_conv_net_base(
     model.add(Dense(nb_classes))
     return model
 
+# FIXME: detection is single bbox only
+PredType = enum.Enum('PredType', 'BINARY REGRESSION DETECTION')
+from keras import backend as K
+def detection_act(t, relu=False):
+    t1 = keras.activations.softmax(t[:, 0:2])
+    t2 = t[:, 2:]
+    if relu:
+        t2 = keras.activations.relu(t2)
+    return K.concatenate([t1, t2], axis=-1)
+
 def generate_conv_net(input_shape, nb_classes,
                       nb_dense=128, nb_filters=32, nb_layers=1, lr_mult=1,
-                      regression=False):
+                      use_bn=True, pred_type=PredType.BINARY):
+
     model = generate_conv_net_base(
             input_shape, nb_classes,
             nb_dense=nb_dense, nb_filters=nb_filters, nb_layers=nb_layers,
-            lr_mult=lr_mult)
-    if not regression:
+            lr_mult=lr_mult, use_bn=use_bn)
+    if pred_type == PredType.BINARY:
         model.add(Activation('softmax'))
+    elif pred_type == PredType.REGRESSION:
+        # clip?
+        pass
+    elif pred_type == PredType.DETECTION:
+        model.add(Activation(detection_act))
     return model
 
 
@@ -107,7 +132,7 @@ class NoScopeModel(object):
             model_fname = os.path.join(output_dir, param_base_fname + '.h5')
             csv_fname = os.path.join(output_dir, param_base_fname + '.csv')
 
-            model = model_gen(*param, regression=self.regression)
+            model = model_gen(*param, pred_type=self.pred_type)
             self.compile_model(model)
 
             train_time = self.train_model(model, X_train, Y_train)
@@ -129,7 +154,7 @@ class NoScopeModel(object):
 class BinaryClassificationModel(NoScopeModel):
     def __init__(self, **kwargs):
         super(BinaryClassificationModel, self).__init__(**kwargs)
-        self.regression = False
+        self.pred_type = Predtype.BINARY
 
     def get_loss(self):
         return 'categorical_crossentropy'
@@ -142,25 +167,17 @@ class BinaryClassificationModel(NoScopeModel):
         begin = time.time()
         # predicted_labels = model.predict_classes(X_test, batch_size=batch_size, verbose=0)
         proba = model.predict(X_test, batch_size=batch_size, verbose=0)
-        predicted_labels = np_utils.probas_to_classes(proba)
+        # predicted_labels = np_utils.probas_to_classes(proba)
         end = time.time()
         test_time = end - begin
 
-        true_labels = np_utils.probas_to_classes(Y_test)
-        precision, recall, fbeta, support = sklearn.metrics.precision_recall_fscore_support(
-                predicted_labels, true_labels)
-        accuracy = sklearn.metrics.accuracy_score(predicted_labels, true_labels)
-
-        num_penalties, thresh_low, thresh_high = \
-            StatsUtils.yolo_oracle(Y_test[:, 1], proba[:, 1])
-        windowed_acc, windowed_supp = StatsUtils.windowed_accuracy(predicted_labels, Y_test)
-        confusion = sklearn.metrics.confusion_matrix(true_labels, predicted_labels)
+        '''confusion = sklearn.metrics.confusion_matrix(true_labels, predicted_labels)
         # Minor smoothing to prevent division by 0 errors
         TN = float(confusion[0][0]) + 1
         FN = float(confusion[1][0]) + 1
         TP = float(confusion[1][1]) + 1
         FP = float(confusion[0][1]) + 1
-        '''metrics = {'recall': TP / (TP + FN),
+        metrics = {'recall': TP / (TP + FN),
                    'specificity': TN / (FP + TN),
                    'precision': TP / (TP + FP),
                    'npv':  TN / (TN + FN),
@@ -170,21 +187,14 @@ class BinaryClassificationModel(NoScopeModel):
                    'accuracy': (TP + TN) / (TP + FP + TN + FN),
                    'f1': (2 * TP) / (2 * TP + FP + FN),
                    'test_time': test_time}'''
-        metrics = {'precision': precision,
-                   'recall': recall,
-                   'fbeta': fbeta,
-                   'support': support,
-                   'accuracy': accuracy,
-                   'penalities': num_penalties,
-                   'windowed_accuracy': windowed_acc,
-                   'windowed_support': windowed_supp,
-                   'test_time': test_time}
+        metrics = Metrics.classification_metrics(proba, Y_test)
+        metrics['test_time'] = test_time
         return metrics
 
 class RegressionModel(NoScopeModel):
     def __init__(self, **kwargs):
         super(RegressionModel, self).__init__(**kwargs)
-        self.regression = True
+        self.pred_type = PredType.REGRESSION
 
     def get_loss(self):
         return 'mean_squared_error'
@@ -193,36 +203,63 @@ class RegressionModel(NoScopeModel):
         return ['mean_squared_error']
 
     def evaluate_model(self, model, X_test, Y_test, batch_size=256):
-        def iou(box1, box2):
-            def to_cent(box):
-                xmin, ymin, xmax, ymax = box
-                xcent = (xmax + xmin) / 2
-                ycent = (ymax + ymin) / 2
-                return (xcent, ycent, xmax - xcent, ymax - ycent)
-            box1 = to_cent(box1)
-            box2 = to_cent(box2)
-            if box1[0] + box1[2] <= box2[0] - box2[2] or \
-                    box2[0] + box2[2] <= box1[0] - box1[2] or \
-                    box1[1] + box1[3] <= box2[1] - box2[3] or \
-                    box2[1] + box2[3] <= box1[1] - box1[3]:
-                return 0.0
-            else:
-                xA = min(box1[0] + box1[2], box2[0] + box2[2])
-                yA = min(box1[1] + box1[3], box2[1] + box2[3])
-                xB = max(box1[0] - box1[2], box2[0] - box2[2])
-                yB = max(box1[1] - box1[3], box2[1] - box2[3])
-                interArea = (xA - xB) * (yA - yB)
-                box1Area = (2 * box1[2]) * (2 * box1[3])
-                box2Area = (2 * box2[2]) * (2 * box2[3])
-                return max(interArea / float(box1Area + box2Area - interArea), 0.0)
         begin = time.time()
         raw_predictions = model.predict(X_test, batch_size=batch_size, verbose=0)
         end = time.time()
         mse = sklearn.metrics.mean_squared_error(Y_test, raw_predictions)
-        iou = [iou(x, y) for x, y in zip(raw_predictions, Y_test)]
+        iou = [Metrics.box_iou(x, y) for x, y in zip(raw_predictions, Y_test)]
         iou = np.array(iou)
 
         metrics = {}
+        metrics['iou'] = np.mean(iou)
+        metrics['mse'] = mse
+        metrics['test_time'] = end - begin
+        return metrics
+
+class DetectionModel(NoScopeModel):
+    def __init__(self, **kwargs):
+        super(DetectionModel, self).__init__(**kwargs)
+        self.pred_type = PredType.DETECTION
+
+    def get_loss(self):
+        def det_loss(y_true, y_pred):
+            yt1 = y_true[:, 0:2]
+            yp1 = y_pred[:, 0:2]
+            yt2 = y_true[:, 2:]
+            yp2 = y_pred[:, 2:]
+            # FIXME: lambda?
+            return keras.losses.categorical_crossentropy(yt1, yp1) + \
+                y_true[:, 1] * keras.losses.mse(yt2, yp2)
+        return det_loss
+
+    def get_metrics(self):
+        def classification_metric(y_true, y_pred):
+            yt = y_true[:, 0:2]
+            yp = y_pred[:, 0:2]
+            return keras.metrics.categorical_accuracy(yt, yp)
+        def box_mse(y_true, y_pred):
+            yt = y_true[:, 2:]
+            yp = y_pred[:, 2:] * K.expand_dims(y_true[:, 1], axis=1)
+            return keras.metrics.mse(yt, yp)
+        def box_iou(y_true, y_pred):
+            yt = y_true[:, 2:]
+            yp = y_pred[:, 2:]
+            return np.mean(map(lambda x, y: Metrics.box_iou(x, y), zip(yt, yp)))
+        return [classification_metric, box_mse]#, box_iou]
+
+    def evaluate_model(self, model, X_test, Y_test, batch_size=256):
+        begin = time.time()
+        Y_pred = model.predict(X_test, batch_size=batch_size, verbose=0)
+        end = time.time()
+
+        box_inds = Y_test[:, 1] == 1
+        Y_pred_box = Y_pred[box_inds, 2:]
+        Y_true_box = Y_test[box_inds, 2:]
+        mse = sklearn.metrics.mean_squared_error(Y_true_box, Y_pred_box)
+        iou = [Metrics.box_iou(x, y) for x, y in zip(Y_true_box, Y_pred_box)]
+
+        proba = Y_pred[:, 0:2]
+        metrics = Metrics.classification_metrics(proba, Y_test[:, 0:2])
         metrics['iou'] = np.mean(iou)
         metrics['mse'] = mse
         metrics['test_time'] = end - begin
@@ -232,7 +269,8 @@ class RegressionModel(NoScopeModel):
 class MulticlassClassificationModel(NoScopeModel):
     def __init__(self, **kwargs):
         super(MulticlassClassificationModel, self).__init__(**kwargs)
-        self.regression = False
+        # FIXME
+        self.pred_type = PredType.BINARY
 
     def get_loss(self):
         return 'categorical_crossentropy'
